8b3ae211216831e133212215bc773fbafda60f18
KARTHIK280 <85799744+KARTHIK280@users.noreply.github.com>
2021-06-13 18:23:27 +0530

    Add files via upload

---
+++ DAY2.py
@@ -0,0 +1,39 @@
+#DAY2
+
+#PRINT STATEMENTS:
+print("hello world");
+print("1,2,3,4,5");
+print('hello google');
+
+
+#***********************************
+
+
+#ASSIGNING STRING TO VARIABLE:
+player = "Kohli"
+print(player)
+
+#VARIABLE OPERATION:
+print(player[0])      #TO PRINT PARTICULAR STRING
+print(player[1:3])    #STRING SLICING
+print(len(player))    #len FUNCTION
+print(player.lower()) #lowerFUNCTION
+print(player.upper()) #upperFUNCTION
+#STRING CONCATENATION:
+a = "Kohli"
+b ="King"
+c = b + a
+print(c)
+c = b +" "+ a   #ADDING SPACE DURING CONCATENATION
+print(c)
+
+
+#***********************************************************
+#CASEFOLD USSAGE:
+text="India Won The Match"
+x=text.casefold()
+print(x)
+print(text.find("n"))
+print(text.isalpha())
+print(text.isalnum())
+
