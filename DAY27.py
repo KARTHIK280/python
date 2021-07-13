f249c9681e897bbee3fee0c745b1d68ec7527093
KARTHIK280 <85799744+KARTHIK280@users.noreply.github.com>
2021-07-13 10:51:37 +0530

    Add files via upload

---
+++ DAY27.py
@@ -0,0 +1,20 @@
+#DAY27
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+print("BestEnlist Python Bootcamp 2021\nDay27\nBalaji.N.R.")
+result = requests.get('https://www.worldometers.info/food-agriculture/india-food-agriculture/')
+sample = BeautifulSoup(result.text, 'lxml')
+count = sample.find_all('div', class_='panel-body')
+data = []
+for i in count:
+    span = i.find('span')
+    data.append(span.string)
+print(data)
+
+# copying content to Excel.
+df = pd.DataFrame({"Indian food and agriculture": data})
+df.index = ['People in india', 'People in the world', 'Forest area in India', 'Cropland in India',
+            'Pesticide use in India', 'Pesticides per hectare']
+df.to_csv('Green India.csv')
